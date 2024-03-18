b088c1456e94337796913f7d1dba54130168ca2d
Himanshu Sharma <96992304+s-Himansh@users.noreply.github.com>
2024-03-18 13:25:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,42 @@ app.use(express.json());
 
 
 app.get('/', (req, res) => {
-	res.send("<h1>API IS Working right on route /sendData</h1>");
+	res.setHeader('Content-type', 'text/html');
+	res.send("<h1>API IS Working right on route /sendData</h1> app.post('/sendData', (req, res) => {
+    try {
+        const data = req.body.data;
+		console.log(data);
+
+		const evenNum = [];
+		const oddNum = [];
+		const alpha = [];
+
+
+		data.forEach((itm) => {
+			if (typeof itm == "number"){
+				if (itm%2 == 0){
+					evenNum.push(itm);
+				}else{
+					oddNum.push(itm);
+				}
+			}else{
+				alpha.push(itm);
+			}
+		});
+
+		return res.status(200).json({
+			"is_success" : true,
+			"user_id" : "sharma", "email" : "abc@gmail.com",
+			"roll_number" : "2110990619",
+			"odd_number" : oddNum,
+			"even_number" : evenNum,
+			"alphabets" : alpha
+		});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: 'Internal server error' });
+    }	
+});");
 });
 
 
