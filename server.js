65f6a3c2d5e94300de7e5d4fde414d90e8f93463
Himanshu Sharma <96992304+s-Himansh@users.noreply.github.com>
2024-03-18 13:16:02 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,58 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+
+
+app.get('/', (req, res) => {
+	res.send("<h1>API IS Working right on route /sendData</h1>");
+});
+
+
+
+app.post('/sendData', (req, res) => {
+    try {
+        const data = req.body.data;
+		console.log(data);
+
+		const evenNum = [];
+		const oddNum = [];
+		const alpha = [];
+
+
+		data.forEach((itm) => {
+			if (typeof itm == "number"){
+				if (itm%2 == 0){
+					evenNum.push(itm);
+				}else{
+					oddNum.push(itm);
+				}
+			}else{
+				alpha.push(itm);
+			}
+		});
+
+		return res.send({
+			"is_success" : true,
+			"user_id" : "sharma", "email" : "abc@gmail.com",
+			"roll_number" : "2110990619",
+			"odd_number" : oddNum,
+			"even_number" : evenNum,
+			"alphabets" : alpha
+		});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: 'Internal server error' });
+    }	
+});
+
+
+
+
+	
+
+app.listen(5000, () => {
+	console.log('app listening at 5000');
+});
